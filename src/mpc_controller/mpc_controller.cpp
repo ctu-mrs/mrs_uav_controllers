c6693f5666f9c160b81af3f0045742440e5e67a4
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-07-12 17:45:56 +0200

    removed baca wind

--- src/mpc_controller/mpc_controller.cpp
+++ src/mpc_controller/mpc_controller.cpp
@@ -594,9 +594,7 @@ const mrs_msgs::AttitudeCommand::ConstPtr MpcController::update(const nav_msgs::
     integral_feedback << Ib_w[0] + Iw_w[0], Ib_w[1] + Iw_w[1], 0;
   }
 
-  Eigen::Vector3d baca_wind(5, 0, 0);
-
-  Eigen::Vector3d f = integral_feedback + feed_forward + baca_wind;
+  Eigen::Vector3d f = integral_feedback + feed_forward;
 
   // | ----------- limiting the downwards acceleration ---------- |
   // the downwards force produced by the position and the acceleration feedback should not be larger than the gravity
