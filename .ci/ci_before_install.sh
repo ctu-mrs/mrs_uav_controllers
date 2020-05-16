b1e4b2267221368488b34b330f6083b778cf65a8
Tomas Baca <klaxalk@gmail.com>
2020-05-16 07:50:48 +0200

    updated CI

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -3,22 +3,35 @@
 set -e
 
 echo "Starting install preparation" 
+
 openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
 eval "$(ssh-agent -s)"
 chmod 600 ./.ci/deploy_key_github
 ssh-add ./.ci/deploy_key_github
+
 sudo apt-get update -qq
 sudo apt-get install dpkg git python-setuptools python3-setuptools python3-pip
 
 echo "clone uav_core"
 cd
-git clone git@github.com:ctu-mrs/uav_core.git
+git clone https://github.com/ctu-mrs/uav_core.git
 cd uav_core
 
 echo "running the main install.sh"
 ./installation/install.sh
 
 gitman update
+
+# get the current commit SHA
+cd "$TRAVIS_BUILD_DIR"
+SHA=`git rev-parse HEAD`
+
+# get the current package name
+PACKAGE_NAME=${PWD##*/}
+
+# checkout the SHA
+cd ~/uav_core/.gitman/$PACKAGE_NAME
+git checkout "$SHA"
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
