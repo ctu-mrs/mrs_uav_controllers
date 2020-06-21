285ab6d52804782c1c6830592e35851b6a86ff3d
Tomas Baca <klaxalk@gmail.com>
2020-06-21 17:36:33 +0200

    update travis for noetic

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -2,6 +2,10 @@
 # author: Robert Penicka
 set -e
 
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
 echo "Starting install preparation" 
 
 openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
@@ -10,6 +14,8 @@ chmod 600 ./.ci/deploy_key_github
 ssh-add ./.ci/deploy_key_github
 
 sudo apt-get update -qq
+sudo apt-mark hold openssh-server
+sudo apt -y upgrade --fix-missing
 sudo apt-get install dpkg git python-setuptools python3-setuptools python3-pip
 
 echo "clone uav_core"
@@ -36,7 +42,7 @@ git checkout "$SHA"
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 ln -s ~/uav_core
-source /opt/ros/melodic/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 cd ~/catkin_ws
 
 echo "install part ended"
--- .ci/ci_test_build.sh
+++ .ci/ci_test_build.sh
@@ -2,8 +2,12 @@
 # author: Robert Penicka
 set -e
 
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
 echo "Starting test build" 
 cd ~/catkin_ws
-source /opt/ros/melodic/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 catkin build
 echo "Ended test build"
