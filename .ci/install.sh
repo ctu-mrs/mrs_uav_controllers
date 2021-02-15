14402155f2852a318cbce5934f9fb014dbcc03e3
Tomas Baca <klaxalk@gmail.com>
2021-02-15 10:27:32 +0100

    updated CI

--- .ci/install.sh
+++ .ci/install.sh
@@ -13,24 +13,7 @@ SHA=`git rev-parse HEAD`
 # get the current package name
 PACKAGE_NAME=${PWD##*/}
 
-sudo apt-get -y update -qq
-sudo apt-mark hold openssh-server # the installation might get stuck while upgrading this
-sudo apt-mark hold msodbcsql17 mssql-tools # microsoft wants to manually accept EULA while upgrading this
-
-# 20.04 problem fix
-sudo apt-get -y install grub-efi
-sudo update-grub
-
-if [ "$distro" = "20.04" ]; then
-# the "gce-compute-image-packages" package often freezes the installation at some point
-# the installation freezes when it tries to manage some systemd services
-# this attempts to install the package and stop the problematic service during the process
-((sleep 90 && (sudo systemctl stop google-instance-setup.service && echo "gce service stoped" || echo "gce service not stoped")) & (sudo timeout 120s apt-get -y install gce-compute-image-packages)) || echo "\e[1;31mInstallation of gce-compute-image-packages failed\e[0m"
-fi
-
-sudo apt-get -y upgrade --fix-missing
-
-sudo apt-get -y install dpkg git
+sudo apt-get -y install git
 
 echo "clone uav_core"
 cd
