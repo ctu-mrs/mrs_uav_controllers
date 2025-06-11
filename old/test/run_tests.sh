efa4cdac27043d941ec0e1fbf2f0f36dff7f1a62
Tomas Baca <Tomas Baca>
2025-06-11 09:25:58 +0200

    removed old stuff

--- old/test/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
-
-catkin test --this -i -p 1 -s
