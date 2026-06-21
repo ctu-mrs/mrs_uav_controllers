980f548a03575825fcb8ddc7629c29d65fde0d3d
Tomas Baca <Tomas Baca>
2026-06-21 10:22:02 +0200

    fixed formatting

--- include/pid.hpp
+++ include/pid.hpp
@@ -12,9 +12,9 @@ private:
   // | ----------------------- parameters ----------------------- |
 
   // gains
-  double _kp_ = 0;  // proportional gain
-  double _kd_ = 0;  // derivative gain
-  double _ki_ = 0;  // integral gain
+  double _kp_ = 0; // proportional gain
+  double _kd_ = 0; // derivative gain
+  double _ki_ = 0; // integral gain
 
   // we remember the last control error, to calculate the difference
   double last_error_ = 0;
@@ -70,9 +70,9 @@ double PIDController::update(const double &error, const double &dt) {
   double difference = (error - last_error_) / dt;
   last_error_       = error;
 
-  double p_component = _kp_ * error;       // proportional feedback
-  double d_component = _kd_ * difference;  // derivative feedback
-  double i_component = _ki_ * integral_;   // derivative feedback
+  double p_component = _kp_ * error;      // proportional feedback
+  double d_component = _kd_ * difference; // derivative feedback
+  double i_component = _ki_ * integral_;  // derivative feedback
 
   double sum = p_component + d_component + i_component;
 
@@ -95,6 +95,6 @@ double PIDController::update(const double &error, const double &dt) {
   return sum;
 }
 
-}  // namespace mrs_uav_controllers
+} // namespace mrs_uav_controllers
 
-#endif  // PID_H
+#endif // PID_H
